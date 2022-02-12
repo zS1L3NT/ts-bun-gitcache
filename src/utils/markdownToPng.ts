7e252a7759940b204d186d7848d1164785cd56c7
Zechariah <zechariahtan144@gmail.com>
2022-02-13 04:56:58 +0800

    Fixed markdownToPng not working when no md file

--- src/utils/markdownToPng.ts
+++ src/utils/markdownToPng.ts
@@ -2,13 +2,20 @@ import axios from "axios"
 import config from "../config.json"
 import convertMd from "convert-md"
 import nodeHtmlToImage from "node-html-to-image"
+import { useTryAsync } from "no-try"
 
-export default async (repoTitle: string) => {
-	const { _content } = await axios
-		.get(`https://raw.githubusercontent.com/${config.github.owner}/${repoTitle}/main/README.md`)
-		.then(res => convertMd(res.data, { type: "html" }))
+export default async (repo: Repo) => {
+	const [err, res] = await useTryAsync(() =>
+		axios
+			.get(
+				`https://raw.githubusercontent.com/${config.github.owner}/${repo.title}/main/README.md`
+			)
+			.then(res => convertMd(res.data, { type: "html" }))
+	)
 
-	const html = _content
+	if (err) return
+
+	const html = res._content
 		.replaceAll("  ", "\t")
 		.replace("<style>", "<style>\n\t\t* {\n\t\t\tcolor: #eee;\n\t\t}\n")
 		.replace("body {", "body {\n\t\t\twidth: 700px;")
@@ -16,8 +23,8 @@ export default async (repoTitle: string) => {
 
 	await nodeHtmlToImage({
 		html,
-		output: `public/${config.github.owner}/${repoTitle}.png`
+		output: `./public/${config.github.owner}/${repo.title}.png`
 	})
 
-	return `${config.host}/${config.github.owner}/${repoTitle}.png`
+	return `${config.host}/${config.github.owner}/${repo.title}.png`
 }
